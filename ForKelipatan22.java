734d3bed89b77f4c83153d145ee18ea43b001710
valinadz <valinaadzra@gmail.com>
2023-10-12 09:01:03 +0700

    jobsheet 7

--- ForKelipatan22.java
+++ ForKelipatan22.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class ForKelipatan22 {
     public static void main (String []args) {
-
         Scanner scan =new Scanner(System.in);
 
         int kelipatan, jumlah, counter;
+        double rataRata;
         jumlah= 0;
         counter= 0;
 
@@ -18,8 +18,13 @@ public class ForKelipatan22 {
                 counter++;
             }
         }
-        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
-
+        if (counter > 0) {
+            rataRata = (double) jumlah / counter;
+            System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+            System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+            System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %2f\n", kelipatan, rataRata);
+        } else {
+            System.out.printf("Tidak ada bilangan kelipatan %d dari 1 sampai 50\n", kelipatan);
+        }
     }
 }
