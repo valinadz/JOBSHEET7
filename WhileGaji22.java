b05aedc63e5a48c42ba2f1c064c28f073f4fe59c
valinadz <valinaadzra@gmail.com>
2023-10-12 08:24:49 +0700

    jobsheet 7

---
+++ WhileGaji22.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class WhileGaji22 {
+    public static void main (String []args) {
+
+        Scanner scan =new Scanner(System.in);
+
+        int jumlahKaryawan, jumlahJamLembur;
+        double gajiLembur, totalGajiLembur;
+        gajiLembur= 0;
+        totalGajiLembur= 0;
+        String jabatan;
+
+        System.out.println("Masukkan jumlah karyawan: ");
+        jumlahKaryawan = scan.nextInt();
+
+        int i = 0;
+
+        while (i < jumlahKaryawan) {
+            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
+            System.out.println("Masukkan jabatan karyawan ke-" + (i+1) + ": ");
+            jabatan = scan.next();
+            System.out.println("Masukkan jumlah jam lembur: ");
+            jumlahJamLembur = scan.nextInt();
+            i++;
+
+            if (jabatan.equalsIgnoreCase("direktur")) {
+                continue;
+            } else if (jabatan.equalsIgnoreCase("manajer")) {
+                gajiLembur = jumlahJamLembur * 100000;
+            }
+            else if (jabatan.equalsIgnoreCase("karyawan")) {
+                gajiLembur = jumlahJamLembur * 75000;
+            }
+
+            totalGajiLembur += gajiLembur;
+            System.out.println("Total gaji lembur: " + totalGajiLembur);
+        }
+    }
+}
